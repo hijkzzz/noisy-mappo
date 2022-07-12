e1f1d97dcb6f1852559e8a95350a0b6226a0f62c
hijkzzz <janhu9527@gmail.com>
2022-07-12 21:57:56 +0800

    Delete scp_results.sh

--- scp_results.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd onpolicy/scripts
-
-# scp -P48763 -r husiyue@140.112.29.204:/home/husiyue/hsy/on-policy/onpolicy/scripts/results .
-# scp -P47777 -r husiyue@140.112.29.193:/home/husiyue/hsy/on-policy/onpolicy/scripts/results .
-scp -P32441 -r hujian@140.112.29.201:/home/hujian/hsy/on-policy/onpolicy/scripts/results .
-
-cd ../../
